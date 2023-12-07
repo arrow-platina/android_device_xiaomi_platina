17d2534b04721e9e6d73e0cf0bb05a0d6d298d5f
Sabar <31942277+sabarop@users.noreply.github.com>
2023-12-07 09:31:00 +0700

    platina: set AAPT resource flavor

--- device.mk
+++ device.mk
@@ -13,6 +13,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 # Project ID Quota
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# AAPT resource flavor
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
 # APEX
 ENABLE_APEX := true
 
