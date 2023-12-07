b5808d3839e0e063de0baf24cfcf9d5ba80c77d2
Sabar <31942277+sabarop@users.noreply.github.com>
2023-12-07 08:57:16 +0700

    platina: commonize build rules and modules

--- device.mk
+++ device.mk
@@ -7,12 +7,6 @@
 # Inherit from sdm660-common
 $(call inherit-product, device/xiaomi/sdm660-common/sdm660.mk)
 
-# Setup dalvik vm configs
-$(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
-
-# Project ID Quota
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # AAPT resource flavor
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
@@ -80,10 +74,6 @@ PRODUCT_SHIPPING_API_LEVEL := 28
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/xiaomi
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
 
 # Wifi
 PRODUCT_PACKAGES += \
